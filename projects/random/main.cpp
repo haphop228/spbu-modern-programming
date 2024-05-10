e727918504da0d67c64c653ad8aa64a557234110
haphop228 <krivoshein.2014@list.ru>
2024-05-10 22:21:37 +0300

    [vector.cpp] push_back, print, get_size are ready

--- projects/random/main.cpp
+++ projects/random/main.cpp
@@ -5,5 +5,6 @@
 int main() {
 	IBusko::Vector<int> vec; // Initialization working
 	std::cout<<"A";
+	vec.print();
 
 }
--- projects/scl/src/vector/vector.cpp
+++ projects/scl/src/vector/vector.cpp
@@ -3,13 +3,56 @@
 
 
 namespace IBusko{
-
 	template <typename T>
 	Vector<T>::~Vector() noexcept{
-		std::cout<<"Delete has started\n";
-		delete [] this->arr;
-		
+		delete [] arr;
 		arr = nullptr;
-		std::cout<<"Arr deleted";
+	}
+	
+	template <typename T>
+	void Vector<T>::push_back(const T& value) noexcept {
+		if (size == capacity){
+			T* new_arr = new T[capacity * 2];
+			for (int i = 0; i < capacity; i++){
+				new_arr[i] = arr[i];
+			}
+			capacity *= 2;
+			arr = new_arr;
+		}
+		arr[size] = value;
+		size++;
+	}
+	
+	template <typename T>
+	bool Vector<T>::has_item(const T& value) const noexcept {
+		return 0;
+	}
+	
+	template <typename T>
+	bool Vector<T>::insert(const int position, const T& value) {
+		return 0;
+	}
+	
+	template <typename T>
+	void Vector<T>::print() const noexcept {
+		if (size == 0){
+			std::cout<< "\nVector is empty";
+		}
+		else{
+			std::cout<< '\n';
+			for (int i = 0; i < size; i++){
+				std::cout<<arr[i] << ' ';
+			}
+		}
+	}
+	
+	template <typename T>
+	bool Vector<T>::remove_first(const T& value) noexcept {
+		return 0;
+	}
+	
+	template <typename T>
+	std::size_t Vector<T>::get_size() const noexcept {
+		return size;
 	}
 }
